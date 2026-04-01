93d3209e3ced9814771f9a636ea53924bcc3ab5d
Leo <161974844+SnickiSnuck@users.noreply.github.com>
2026-04-01 17:14:26 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleCover(colIndex) {
-    // Wir suchen alle td-Elemente, die an der Position colIndex in ihrer Zeile stehen
-    const cells = document.querySelectorAll(`tbody td:nth-child(${colIndex})`);
-    
-    cells.forEach(td => {
-        td.classList.toggle('covered');
-    });
-}
